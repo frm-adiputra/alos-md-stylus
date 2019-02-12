aadc9bd76571fd6f99750fcbac199e334ba10492
Firmansyah Adiputra <frm.adiputra@gmail.com>
2019-02-12 07:32:50 +0700

    Add for Vuetify

--- test/server.js
+++ test/server.js
@@ -10,7 +10,7 @@ app.set('view engine', 'pug')
 
 app.use(stylus.middleware({
 	src: __dirname + '/../src',
-	dest: __dirname + '/../dist',
+	dest: __dirname + '/../test-dist',
 	compile: (str, path) => {
 		return stylus(str)
 			.set('filename', path)
@@ -23,7 +23,7 @@ app.get('/', function (req, res) { res.render('index') })
 app.get('/typography.html', function (req, res) { res.render('typography') })
 app.get('/font-metrics.html', function (req, res) { res.render('font-metrics') })
 
-app.use(express.static('dist'))
+app.use(express.static(__dirname + '/../test-dist'))
 app.use(express.static(__dirname + '/static'))
 
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}`))
